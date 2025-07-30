6dbc9e7d8f043cc6148f0a3183aeda54e450cf1f
wacul123 <waculsamsul@gmail.com>
2025-07-30 23:29:16 +0700

    Fix credentials loading to use environment variables

--- server.js
+++ server.js
@@ -29,25 +29,21 @@ const SCOPES = [
   'https://www.googleapis.com/auth/spreadsheets',
   'https://www.googleapis.com/auth/drive.file',
 ];
-// For local development, use credentials.json
-let credentials;
-if (process.env.NODE_ENV === 'production') {
-    credentials = {
-        type: "service_account",
-        project_id: process.env.PROJECT_ID,
-        private_key_id: process.env.PRIVATE_KEY_ID,
-        private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-        client_email: process.env.CLIENT_EMAIL,
-        client_id: process.env.CLIENT_ID,
-        auth_uri: "https://accounts.google.com/o/oauth2/auth",
-        token_uri: "https://oauth2.googleapis.com/token",
-        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-        client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
-        universe_domain: "googleapis.com"
-    };
-} else {
-    credentials = require('./credentials.json');
-}
+
+const credentials = {
+    type: "service_account",
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+    universe_domain: "googleapis.com"
+};
+
 const SPREADSHEET_ID = '1BNhyJfE2ejqAAXes1gz6HaBd2KijtG86xkGA1AbxXDY';
 const DRIVE_FOLDER_ID = '1aqeH-9T7ttRSxL1OwMIVm7uc_AYT6nzQ';
 
